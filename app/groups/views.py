02b43235042be411f142f5026355c84aada71df7
Erin Lewis <eplewis89@gmail.com>
2022-05-11 12:51:06 -0700

    group query: added filtering to group

--- app/groups/views.py
+++ app/groups/views.py
@@ -65,7 +65,42 @@ def querygroups():
     Query all the groups in /etc/groups for some specific info
     :return:
     """
-
     req = request.args.to_dict()
 
-    return response('success', req, 200)
+    print(req)
+
+    file_object = open(file, 'r')
+    group_contents = []
+
+    for line in file_object:
+        line = line.strip()
+        fields = line.split(":")
+        
+        group = {
+            "name" : fields[0],
+            "gid" : fields[2],
+            "members" : fields[3].split(',')
+        }
+
+        add = True
+
+        for x in req:
+            if req[x] is None:
+                add = False
+                continue
+
+            if len(group[x]) > 0:
+                members = req[x].split(',')
+
+                for m in members:
+                    if m not in group[x]:
+                        add = False
+            elif group[x] != req[x]:
+                add = False
+
+        if add:
+            group_contents.append(group)
+
+    file_object.close()
+
+    return jsonify(group_contents)
